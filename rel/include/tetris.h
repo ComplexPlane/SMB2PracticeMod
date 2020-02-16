efba794157c7fd510b62644bb192240afdccb09e
ComplexPlane <complexplanegames@gmail.com>
2020-02-16 14:12:00 -0500

    Draw tetris info text

--- rel/include/tetris.h
+++ rel/include/tetris.h
@@ -29,15 +29,20 @@ private:
     State m_state;
     Cell m_board[BOARD_WIDTH][BOARD_HEIGHT];
 
+    int m_score;
+    int m_highScore;
+
     Cell getRandomCell();
     Tetrad getRandomTetrad();
 
     void draw();
     void drawAsciiRect(int x, int y, int widthChars, int heightChars, uint8_t color);
     void drawRect(float x1, float y1, float x2, float y2, gc::GXColor color);
-    void drawAsciiWindow(int x, int y, int widthChars, int heightChars);
+    void drawAsciiWindow();
     void drawGrid();
     void drawTextPalette();
+    void drawInfoText();
+    void drawDebugTextPrintf(int x, int y, uint8_t color, const char *format, ...);
 };
 
 }
--- rel/tetris.cpp
+++ rel/tetris.cpp
@@ -8,7 +8,9 @@
 #include "global.h"
 
 #include <cstring>
+#include <cstdio>
 #include <cstdlib>
+#include <cstdarg>
 
 static constexpr int NUM_TETRADS = 7;
 static constexpr int NUM_CELL_TYPES = 8;
@@ -31,7 +33,7 @@ static constexpr char BOXCHAR_DARROW = '\x1f';
 
 static constexpr int CHAR_WIDTH = 0xc;
 
-static constexpr uint8_t CELL_ALPHA = 0xc0;
+static constexpr uint8_t CELL_ALPHA = 0xff;
 
 static const gc::GXColor CELL_COLORS[NUM_CELL_TYPES] = {
     {0x02, 0xf0, 0xed, CELL_ALPHA}, // I
@@ -48,6 +50,8 @@ namespace mod {
 
 void Tetris::init() {
     m_state = State::HIDDEN;
+    m_score = 0;
+    m_highScore = 1000;
 
     for (int x = 0; x < BOARD_WIDTH; x++) {
         for (int y = 0; y < BOARD_HEIGHT; y++) {
@@ -94,8 +98,9 @@ void Tetris::draw() {
     gc::GXColor tev1Color = {0, 0, 0, 0};
     gc::GXSetTevColor(gc::GX_TEVREG1, tev1Color);
 
-    drawAsciiWindow(87, 8, 30, 36);
+    drawAsciiWindow();
     drawGrid();
+    drawInfoText();
 }
 
 void Tetris::drawAsciiRect(int xpos, int ypos, int xchars, int ychars, uint8_t color) {
@@ -106,7 +111,7 @@ void Tetris::drawAsciiRect(int xpos, int ypos, int xchars, int ychars, uint8_t c
     mkb::drawDebugTextCharEn(xpos, ypos + (ychars - 1) * CHAR_WIDTH, BOXCHAR_DL, color);
 
     constexpr int X_VDIV = 16;
-    constexpr int Y_HDIV = 20;
+    constexpr int Y_HDIV = 24;
 
     // Draw horizontal lines
     for (int i = 1; i < xchars - 1; i++) {
@@ -163,22 +168,26 @@ void Tetris::drawRect(float x1, float y1, float x2, float y2, gc::GXColor color)
     gc::GXTexCoord2f32(0, 1);
 }
 
-void Tetris::drawAsciiWindow(int x, int y, int widthChars, int heightChars) {
+void Tetris::drawAsciiWindow() {
+    constexpr int X = 130;
+    constexpr int Y = 8;
+    constexpr int WIDTH_CHARS = 30;
+    constexpr int HEIGHT_CHARS = 36;
     constexpr int MARGIN = 5;
     constexpr float YSCALE = 1.07142857; // Magic scalar found in decompile
 
-    float startx = x + MARGIN;
-    float starty = y * YSCALE + MARGIN;
+    float startx = X + MARGIN;
+    float starty = Y * YSCALE + MARGIN;
 
-    float endx = x + widthChars * CHAR_WIDTH - MARGIN;
-    float endy = (y + heightChars * CHAR_WIDTH) * YSCALE - MARGIN;
+    float endx = X + WIDTH_CHARS * CHAR_WIDTH - MARGIN;
+    float endy = (Y + HEIGHT_CHARS * CHAR_WIDTH) * YSCALE - MARGIN;
 
     drawRect(startx, starty, endx, endy, {0x00, 0x00, 0x00, 0x80});
-    drawAsciiRect(x, y, widthChars, heightChars, 0b01001110);
+    drawAsciiRect(X, Y, WIDTH_CHARS, HEIGHT_CHARS, 0b01001110);
 }
 
 void Tetris::drawGrid() {
-    constexpr int DRAWX_START = 100;
+    constexpr int DRAWX_START = 143;
     constexpr int DRAWY_START = 25;
 
     for (int x = 0; x < BOARD_WIDTH; x++) {
@@ -202,6 +211,35 @@ void Tetris::drawTextPalette() {
         int x = c % 16 * CHAR_WIDTH;
         int y = c / 16 * CHAR_WIDTH;
         mkb::drawDebugTextCharEn(x, y, c, c * 2);
+    }
+}
+
+void Tetris::drawInfoText() {
+    constexpr int STARTX = 335;
+    constexpr int STARTY = 310;
+
+    drawDebugTextPrintf(STARTX, STARTY, 0b00101111, "SCORE");
+    drawDebugTextPrintf(STARTX, STARTY + 16, 0xff, "%d", m_score);
+
+    drawDebugTextPrintf(STARTX, STARTY + 50, 0b01110111, "HIGH SCORE");
+    drawDebugTextPrintf(STARTX, STARTY + 50 + 16, 0xff, "%d", m_highScore);
+
+    drawDebugTextPrintf(429, 283, 0b11100011, "NEXT");
+}
+
+void Tetris::drawDebugTextPrintf(int x, int y, uint8_t color, const char *format, ...) {
+    va_list args;
+    va_start(args, format);
+
+    // Shouldn't be able to print a string to the screen longer than this
+    // Be careful not to overflow! MKB2 doesn't have vsnprintf
+    static char buf[80];
+    vsprintf(buf, format, args);
+
+    va_end(args);
+
+    for (int i = 0; buf[i] != '\0'; i++) {
+        mkb::drawDebugTextCharEn(x + i * CHAR_WIDTH, y, buf[i], color);
     }
 }
 
