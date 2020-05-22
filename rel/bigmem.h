abd1372421e8d1e9015da92baf9f92f9a78eb493
ComplexPlane <complexplanegames@gmail.com>
2020-05-22 17:35:36 -0400

    Remove some old bigmem/savestate experimental stuff

--- rel/bigmem.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-namespace bigmem {
-
-void init();
-void createGameHeapsUsingExtraMem();
-
-}
--- rel/mod.cpp
+++ rel/mod.cpp
@@ -3,7 +3,6 @@
 #include "patch.h"
 #include "global.h"
 #include "pad.h"
-#include "bigmem.h"
 
 #include <gc/gc.h>
 #include <mkb/mkb.h>
@@ -37,8 +36,7 @@ void Mod::init() {
   // Nop the conditional that guards `drawDebugText`, enabling it even when debug mode is disabled
   patch::writeNop(reinterpret_cast<void *>(0x80299f54));
 
-//  global::tetris.init();
-  bigmem::init();
+  global::tetris.init();
 
   global::drawDebugText_trampoline = patch::hookFunction(
       mkb::drawDebugText,
@@ -47,7 +45,7 @@ void Mod::init() {
         // Gets run at the start of smb2's function which draws debug text windows,
         // which is called at the end of smb2's function which draws the UI in general.
 
-//        global::tetris.update();
+        global::tetris.update();
 
         global::drawDebugText_trampoline();
       });
@@ -91,24 +89,24 @@ void Mod::init() {
 //    return global::OSAllocFromHeap_trampoline(heap, size);
 //  });
 
-  global::OSCreateHeap_trampoline = patch::hookFunction(
-      gc::OSCreateHeap,
-      [](void *start, void *end) {
-        gc::OSHeapHandle ret = global::OSCreateHeap_trampoline(start, end);
-        gc::OSReport("[mod] OSCreateHeap(0x%08X, 0x%08X) -> %d (size 0x%08X)\n",
-            start,
-            end,
-            ret,
-            reinterpret_cast<uint32_t>(end) - reinterpret_cast<uint32_t>(start));
-        return ret;
-      });
+//  global::OSCreateHeap_trampoline = patch::hookFunction(
+//      gc::OSCreateHeap,
+//      [](void *start, void *end) {
+//        gc::OSHeapHandle ret = global::OSCreateHeap_trampoline(start, end);
+//        gc::OSReport("[mod] OSCreateHeap(0x%08X, 0x%08X) -> %d (size 0x%08X)\n",
+//            start,
+//            end,
+//            ret,
+//            reinterpret_cast<uint32_t>(end) - reinterpret_cast<uint32_t>(start));
+//        return ret;
+//      });
 
-  global::OSDestroyHeap_trampoline = patch::hookFunction(
-      gc::OSDestroyHeap,
-      [](gc::OSHeapHandle heap) {
-        gc::OSReport("[mod] OSDestroyHeap(%d)\n", heap);
-        global::OSDestroyHeap_trampoline(heap);
-      });
+//  global::OSDestroyHeap_trampoline = patch::hookFunction(
+//      gc::OSDestroyHeap,
+//      [](gc::OSHeapHandle heap) {
+//        gc::OSReport("[mod] OSDestroyHeap(%d)\n", heap);
+//        global::OSDestroyHeap_trampoline(heap);
+//      });
 //
 //  global::OSFreeToHeap_trampoline = patch::hookFunction(
 //      gc::OSFreeToHeap,
@@ -117,12 +115,12 @@ void Mod::init() {
 //        global::OSFreeToHeap_trampoline(heap, ptr);
 //      });
 //
-  global::OSInitAlloc_trampoline = patch::hookFunction(
-      gc::OSInitAlloc,
-      [](void *arenaStart, void *arenaEnd, int maxHeaps) {
-        gc::OSReport("[mod] OSInitAlloc(0x%08X, 0x%08X, %d)\n", arenaStart, arenaEnd, maxHeaps);
-        return global::OSInitAlloc_trampoline(arenaStart, arenaEnd, maxHeaps);
-      });
+//  global::OSInitAlloc_trampoline = patch::hookFunction(
+//      gc::OSInitAlloc,
+//      [](void *arenaStart, void *arenaEnd, int maxHeaps) {
+//        gc::OSReport("[mod] OSInitAlloc(0x%08X, 0x%08X, %d)\n", arenaStart, arenaEnd, maxHeaps);
+//        return global::OSInitAlloc_trampoline(arenaStart, arenaEnd, maxHeaps);
+//      });
 //
 //  global::OSSetCurrentHeap_trampoline = patch::hookFunction(
 //      gc::OSSetCurrentHeap,
@@ -143,55 +141,37 @@ void Mod::init() {
 //  patch::writeBranch(reinterpret_cast<void *>(0x80064cd0), reinterpret_cast<void *>(0x80064cf0));
 //  patch::writeBranchBL(reinterpret_cast<void *>(0x80064cf4), reinterpret_cast<void *>(getLockedCache));
 
-  global::OSSetArenaLo_trampoline = patch::hookFunction(
-      gc::OSSetArenaLo,
-      [](void *newLo) {
-        void *oldLo = gc::OSGetArenaLo();
-        gc::OSReport("[mod] OSSetArenaLo(0x%08X) (old: 0x%08X)\n", newLo, oldLo);
-        global::OSSetArenaLo_trampoline(newLo);
-      });
-
-  global::OSSetArenaHi_trampoline = patch::hookFunction(
-      gc::OSSetArenaHi,
-      [](void *newHi) {
-        void *oldHi = gc::OSGetArenaHi();
-        gc::OSReport("[mod] OSSetArenaHi(0x%08X) (old: 0x%08X)\n", newHi, oldHi);
-        global::OSSetArenaHi_trampoline(newHi);
-      });
-
-  global::OSAllocFromArenaLo_trampoline = patch::hookFunction(
-      gc::OSAllocFromArenaLo,
-      [](uint32_t size, uint32_t align) {
-        void *ret = global::OSAllocFromArenaLo_trampoline(size, align);
-        gc::OSReport("[mod] OSAllocFromArenaLo(0x%08X, 0x%08X) -> 0x%08X\n", size, align, ret);
-        return ret;
-      });
-
-  global::OSAllocFromArenaHi_trampoline = patch::hookFunction(
-      gc::OSAllocFromArenaHi,
-      [](uint32_t size, uint32_t align) {
-        void *ret = global::OSAllocFromArenaHi_trampoline(size, align);
-        gc::OSReport("[mod] OSAllocFromArenaHi(0x%08X, 0x%08X) -> 0x%08X\n", size, align, ret);
-        return ret;
-      });
-
-  global::createGameHeaps_trampoline = patch::hookFunction(
-      mkb::createGameHeaps,
-      [](int param1) {
-        if (param1 == 0)
-        {
-          gc::OSReport("[mod] Begin createGameHeaps bigmem override\n");
-          bigmem::createGameHeapsUsingExtraMem();
-          gc::OSReport("[mod] End createGameHeaps bigmem override\n");
-        }
-        else
-        {
-          gc::OSReport("[mod] Begin createGameHeaps(0x%08X)\n", param1);
-          global::createGameHeaps_trampoline(param1);
-          gc::OSReport("[mod] End createGameHeaps(0x%08X)\n", param1);
-        }
-      });
-
+//  global::OSSetArenaLo_trampoline = patch::hookFunction(
+//      gc::OSSetArenaLo,
+//      [](void *newLo) {
+//        void *oldLo = gc::OSGetArenaLo();
+//        gc::OSReport("[mod] OSSetArenaLo(0x%08X) (old: 0x%08X)\n", newLo, oldLo);
+//        global::OSSetArenaLo_trampoline(newLo);
+//      });
+//
+//  global::OSSetArenaHi_trampoline = patch::hookFunction(
+//      gc::OSSetArenaHi,
+//      [](void *newHi) {
+//        void *oldHi = gc::OSGetArenaHi();
+//        gc::OSReport("[mod] OSSetArenaHi(0x%08X) (old: 0x%08X)\n", newHi, oldHi);
+//        global::OSSetArenaHi_trampoline(newHi);
+//      });
+//
+//  global::OSAllocFromArenaLo_trampoline = patch::hookFunction(
+//      gc::OSAllocFromArenaLo,
+//      [](uint32_t size, uint32_t align) {
+//        void *ret = global::OSAllocFromArenaLo_trampoline(size, align);
+//        gc::OSReport("[mod] OSAllocFromArenaLo(0x%08X, 0x%08X) -> 0x%08X\n", size, align, ret);
+//        return ret;
+//      });
+//
+//  global::OSAllocFromArenaHi_trampoline = patch::hookFunction(
+//      gc::OSAllocFromArenaHi,
+//      [](uint32_t size, uint32_t align) {
+//        void *ret = global::OSAllocFromArenaHi_trampoline(size, align);
+//        gc::OSReport("[mod] OSAllocFromArenaHi(0x%08X, 0x%08X) -> 0x%08X\n", size, align, ret);
+//        return ret;
+//      });
 }
 
 }
