980a3dcdbd0b2fb74881b172b49ed1a1cb433eb9
ComplexPlane <complexplanegames@gmail.com>
2020-06-10 19:58:39 -0400

    Add start of memsave stuff

--- rel/include/memsave.h
+++ rel/include/memsave.h
@@ -1,1 +1,38 @@
 #pragma once
+
+#include <cstdint>
+
+namespace memsave
+{
+
+class MemSave
+{
+public:
+    MemSave();
+    ~MemSave();
+
+    void enterPreallocMode();
+    bool allocSaveBuf();
+    void enterSaveStateMode();
+
+    void saveRegion(void *ptr, uint32_t size);
+    void loadState();
+
+private:
+    struct RegionInfo
+    {
+        void *ptr;
+        uint32_t size;
+    };
+
+    uint8_t *m_saveBuf;
+    RegionInfo *m_schema; // List of region metadata
+    uint32_t m_regionCount;
+    bool m_preallocMode;
+    uint32_t m_saveBufPos;
+
+    void clear();
+
+};
+
+}
--- rel/memsave.cpp
+++ rel/memsave.cpp
@@ -1,4 +1,62 @@
-//
-// Created by alex on 6/10/20.
-//
+#include "memsave.h"
 
+namespace memsave
+{
+
+MemSave::MemSave() :
+    m_saveBuf{nullptr},
+    m_regionCount{0},
+    m_schema{nullptr},
+    m_preallocMode{true} {}
+
+MemSave::~MemSave()
+{
+    clear();
+}
+
+void MemSave::enterPreallocMode()
+{
+    clear();
+    m_preallocMode = true;
+}
+
+bool MemSave::allocSaveBuf()
+{
+    return false;
+}
+
+void MemSave::enterSaveStateMode()
+{
+    m_preallocMode = false;
+}
+
+void MemSave::saveRegion(void *ptr, uint32_t size)
+{
+    if (m_preallocMode)
+    {
+        m_saveBufPos += size;
+        m_regionCount++;
+    }
+}
+
+void MemSave::loadState()
+{
+
+}
+
+void MemSave::clear()
+{
+    if (m_saveBuf)
+    {
+        delete[] m_saveBuf;
+        m_saveBuf = 0;
+    }
+    if (m_schema)
+    {
+        delete[] m_schema;
+        m_schema = 0;
+    }
+    m_regionCount = 0;
+}
+
+}
