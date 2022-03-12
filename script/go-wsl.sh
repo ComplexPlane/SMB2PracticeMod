d2fbee2a3705ae60a62c7737278555654c64a996
ComplexPlane <complexplanegames@gmail.com>
2022-03-12 17:26:16 -0500

    Basic raw input display

--- script/go-wsl.sh
+++ script/go-wsl.sh
@@ -13,22 +13,22 @@ RELLOADER_SRC=relloader/src/iso-rel-loader-us.asm
 #     "$PPCINJECT" "$DOL_SRC" "$DOL_DEST" "$RELLOADER_SRC"
 # fi
 
-mkdir -p /c/Users/ComplexPlane/Documents/Dolphin\ Emulator/GC/USA/Card\ B/
+mkdir -p /mnt/c/Users/ComplexPlane/Documents/Dolphin\ Emulator/GC/USA/Card\ B/
 
 if [[ "${1:-}" == "-a" ]]; then
   echo 'Building all versions'
   make -j"$(nproc)"
 
   echo 'Copying GCIs to dolphin emu folder'
-  cp ApeSphere.*.gci /c/Users/ComplexPlane/Documents/Dolphin\ Emulator/GC/USA/Card\ B/
+  cp SMB2PracticeMod.*.gci /mnt/c/Users/ComplexPlane/Documents/Dolphin\ Emulator/GC/USA/Card\ B/
 
 else
   echo 'Building us version'
   make us -j"$(nproc)"
 
   echo 'Copying GCI to dolphin emu folder'
-  cp ApeSphere.GM2E8P.gci /c/Users/ComplexPlane/Documents/Dolphin\ Emulator/GC/USA/Card\ B/
+  cp SMB2PracticeMod.GM2E8P.gci /mnt/c/Users/ComplexPlane/Documents/Dolphin\ Emulator/GC/USA/Card\ B/
 fi
 
 # echo 'Copying REL to smb2mut'
-# cp ApeSphere.us.rel ~/romhack/smb2mut/files/mkb2.rel_sample.rel
+# cp SMB2PracticeMod.us.rel ~/romhack/smb2mut/files/mkb2.rel_sample.rel
