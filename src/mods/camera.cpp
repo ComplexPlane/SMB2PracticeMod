88b328f92e08dd7479a0f0cfc21c80229274cad9
rehtrop <kingretrop@gmail.com>
2023-09-30 16:02:51 -0500

    assist fixes, camera enum added

--- src/mods/camera.cpp
+++ src/mods/camera.cpp
@@ -8,17 +8,23 @@
 
 namespace camera {
 
-static bool s_prev_value;
+enum class CameraType {
+    Default = 0,
+    ForceSMB2 = 1,
+    ForceSMB1 = 2,
+};
+
+static CameraType s_prev_value;
 
 void init() {}
 
 void tick() {
-    u8 value = pref::get(pref::U8Pref::Camera);
+    CameraType value = CameraType(pref::get(pref::U8Pref::Camera));
 
     switch (value) {
-        case 0: {
+        case CameraType::Default: {
             if (s_prev_value != value) {
-                // restore cam to smb2 once
+                // restore cam to smb2 once (so toggle still works)
                 if (mkb::cameras[0].mode == 0x1) {
                     mkb::cameras[0].mode = 0x4c;
                 }
@@ -30,7 +36,7 @@ void tick() {
             }
             break;
         }
-        case 1: {  // force smb2 cam
+        case CameraType::ForceSMB2: {
             // write every frame to force the values
             if (mkb::cameras[0].mode == 0x1) {
                 mkb::cameras[0].mode = 0x4c;
@@ -42,11 +48,12 @@ void tick() {
             mkb::camera_height = 0.8;
             break;
         }
-        case 2: {  // force smb1 cam
+        case CameraType::ForceSMB1: {
             // write every frame to force the values
             if (mkb::cameras[0].mode == 0x4c) {
                 mkb::cameras[0].mode = 0x1;
             }
+            // Explanation from bomb in WS
             // The write_word statement moves the camera's angle down by 2.8 degrees to match SMB1's
             // angle. Everything else brings the camera position/pivot values in-line with SMB1's
             // values Camera mode 0x1 enables SMB1-like vertical camera tracking, camera mode 0x4c
@@ -55,9 +62,6 @@ void tick() {
             mkb::g_camera_turn_rate_scale = 0.6875;
             mkb::camera_pivot_height = -0.5;
             mkb::camera_height = 1;
-            break;
-        }
-        case 3: {  // TODO: custom values
             break;
         }
     }
--- src/mods/fallout.cpp
+++ src/mods/fallout.cpp
@@ -1,106 +1,118 @@
 #include "fallout.h"
-#include "../mods/freecam.h"
-#include "../systems/pref.h"
-#include "../utils/macro_utils.h"
-#include "../utils/patch.h"
+#include "mkb/mkb2_ghidra.h"
+#include "mods/freecam.h"
+#include "systems/pref.h"
+#include "utils/macro_utils.h"
+#include "utils/patch.h"
 
 namespace fallout {
 
-static patch::Tramp<decltype(&mkb::did_ball_fallout)> s_did_ball_fallout;
+enum class TimerType {
+    Default,
+    FreezeInstantly,
+    FreezeAtZero,
+    CountUpwards,
+    Invalid  // this state is used to determine when
+};
 
-static u8 s_prev_pref;
-static u8 s_prev_freecam = 255;
+static patch::Tramp<decltype(&mkb::did_ball_fallout)> s_did_ball_fallout_tramp;
 
-static bool s_halted;  // timer cannot move
+static TimerType s_prev_pref = TimerType::Invalid;
+static TimerType s_prev_freecam = TimerType::Invalid;
+
+static bool s_halted;  // freeze timer for TimerType::FreezeAtZero
 
 void init() {
-    u8 current_pref = pref::get(pref::U8Pref::TimerType);
-    if (current_pref == 1) {  // freeze at max
-        // time over at 0 frames
-        patch::write_word(reinterpret_cast<void*>(0x80297548), 0x2c000000);
-        // add 0 to timer each frame (timer doesnt move)
-        patch::write_word(reinterpret_cast<void*>(0x80297534), 0x38030000);
-    } else if (current_pref == 2) {  // freeze at 0.00
-        // time over at -60 frames (so timer is able to stop at 0.00)
-        patch::write_word(reinterpret_cast<void*>(0x80297548), 0x2c00ffa0);
-        // add -1 to timer each frame (will need to freeze timer at 0.00 and unfreeze on retry)
-        patch::write_word(reinterpret_cast<void*>(0x80297534), 0x3803ffff);
-    } else if (current_pref == 3) {  // count up
-        // time over at -60 frames (so timer is able to stop at 0.00)
-        patch::write_word(reinterpret_cast<void*>(0x80297548), 0x2c00ffa0);
-        // add 1 to timer each frame
-        patch::write_word(reinterpret_cast<void*>(0x80297534), 0x38030001);
-    }
-
     // stop fallouts
-    patch::hook_function(s_did_ball_fallout, mkb::did_ball_fallout, [](mkb::Ball* ball) {
+    patch::hook_function(s_did_ball_fallout_tramp, mkb::did_ball_fallout, [](mkb::Ball* ball) {
         if (pref::get(pref::BoolPref::DisableFallouts)) {
-            return (mkb::BOOL32) false;
+            return static_cast<mkb::BOOL32>(false);
         }
 
-        return (mkb::BOOL32)s_did_ball_fallout.dest(ball);
+        return static_cast<mkb::BOOL32>(s_did_ball_fallout_tramp.dest(ball));
     });
 }
 
 void freeze_timer() {
-    u8 current_pref = pref::get(pref::U8Pref::TimerType);
-    bool change = current_pref != s_prev_pref;
-    if (current_pref == 0 && change) {  // default
-        // time over at 0 frames
-        patch::write_word(reinterpret_cast<void*>(0x80297548), 0x2c000000);
-        // add -1 to timer each frame
-        patch::write_word(reinterpret_cast<void*>(0x80297534), 0x3803ffff);
-    } else if (current_pref == 1 && change) {  // freeze at max
-        // time over at 0 frames
-        patch::write_word(reinterpret_cast<void*>(0x80297548), 0x2c000000);
-        // add 0 to timer each frame (timer doesnt move)
-        patch::write_word(reinterpret_cast<void*>(0x80297534), 0x38030000);
-    } else if (current_pref == 2 && change) {  // freeze at 0.00
-        // time over at -60 frames (so timer is able to stop at 0.00)
-        patch::write_word(reinterpret_cast<void*>(0x80297548), 0x2c00ffa0);
-        // add -1 to timer each frame (will need to freeze timer at 0.00 and unfreeze on retry)
-        patch::write_word(reinterpret_cast<void*>(0x80297534), 0x3803ffff);
-    } else if (current_pref == 2 && mkb::mode_info.stage_time_frames_remaining == 0) {
-        // freeze timer at 0.00
-        patch::write_word(reinterpret_cast<void*>(0x80297534), 0x38030000);
-        s_halted = true;
-    } else if (current_pref == 2 && mkb::mode_info.stage_time_frames_remaining != 0 && s_halted) {
-        // unfreeze timer when not 0.00
-        patch::write_word(reinterpret_cast<void*>(0x80297534), 0x3803ffff);
-        s_halted = false;
-    } else if (current_pref == 3 && change) {  // count up
-        // time over at -60 frames (so timer is able to stop at 0.00)
-        patch::write_word(reinterpret_cast<void*>(0x80297548), 0x2c00ffa0);
-        // add 1 to timer each frame
-        patch::write_word(reinterpret_cast<void*>(0x80297534), 0x38030001);
-    }
-
-    if (current_pref == 3 && mkb::sub_mode == mkb::SMD_GAME_READY_INIT) {
-        mkb::mode_info.stage_time_frames_remaining = 0;
-    }
+    TimerType current_pref = TimerType(pref::get(pref::U8Pref::TimerType));
+    bool update_timer_incr =
+        mkb::sub_mode == mkb::SMD_GAME_READY_INIT || current_pref != s_prev_pref;
     s_prev_pref = current_pref;
+
+    switch (current_pref) {
+        case TimerType::Default: {
+            // time over at 0 frames
+            *reinterpret_cast<u32*>(0x80297548) = 0x2c000000;
+            // add -1 to timer each frame
+            if (update_timer_incr) {
+                patch::write_word(reinterpret_cast<u32*>(0x80297534), 0x3803ffff);
+            }
+            break;
+        }
+        case TimerType::FreezeInstantly: {
+            // time over at 0 frames
+            *reinterpret_cast<u32*>(0x80297548) = 0x2c000000;
+            // add 0 to timer each frame (timer doesnt move)
+            if (update_timer_incr) {
+                patch::write_word(reinterpret_cast<u32*>(0x80297534), 0x38030000);
+            }
+            break;
+        }
+        case TimerType::FreezeAtZero: {
+            // time over at -60 frames (so timer is able to stop at 0.00)
+            *reinterpret_cast<u32*>(0x80297548) = 0x2c00ffa0;
+            // add -1 to timer each frame (will need to freeze timer at 0.00 and unfreeze on retry)
+            if (update_timer_incr) {
+                patch::write_word(reinterpret_cast<u32*>(0x80297534), 0x3803ffff);
+            }
+
+            // when timer hits 0, add 0 to timer each frame
+            if (mkb::mode_info.stage_time_frames_remaining == 0 && !s_halted) {
+                patch::write_word(reinterpret_cast<u32*>(0x80297534), 0x38030000);
+                s_halted = true;
+            }
+            // when timer is reset on retry, add -1 to timer each frame
+            else if (mkb::mode_info.stage_time_frames_remaining != 0 && s_halted) {
+                patch::write_word(reinterpret_cast<u32*>(0x80297534), 0x3803ffff);
+                s_halted = false;
+            }
+            break;
+        }
+        case TimerType::CountUpwards: {
+            if (mkb::sub_mode == mkb::SMD_GAME_READY_INIT) {
+                mkb::mode_info.stage_time_frames_remaining = 0;
+            }
+            // time over at -60 frames (so timer is able to stop at 0.00)
+            *reinterpret_cast<u32*>(0x80297548) = 0x2c00ffa0;
+            // add 1 to timer each frame
+            if (update_timer_incr) {
+                patch::write_word(reinterpret_cast<u32*>(0x80297534), 0x38030001);
+            }
+            break;
+        }
+        case TimerType::Invalid: {
+            break;
+        }
+    }
 }
 
 void tick() {
-    if (freecam::should_freeze_timer() && s_prev_freecam == 255) {
-        s_prev_freecam = pref::get(pref::U8Pref::TimerType);
+    if (freecam::should_freeze_timer() && s_prev_freecam == TimerType::Invalid) {
+        s_prev_freecam = TimerType(pref::get(pref::U8Pref::TimerType));
         pref::set(pref::U8Pref::TimerType, 1);
-    } else if (!freecam::should_freeze_timer() && s_prev_freecam != 255) {
-        pref::set(pref::U8Pref::TimerType, s_prev_freecam);
-        s_prev_freecam = 255;
+    } else if (!freecam::should_freeze_timer() && s_prev_freecam != TimerType::Invalid) {
+        pref::set(pref::U8Pref::TimerType, static_cast<u8>(s_prev_freecam));
+        s_prev_freecam = TimerType::Invalid;
     }
     freeze_timer();
 
     if (pref::get(pref::BoolPref::DisableFallouts) &&
-        pref::get(pref::BoolPref::BouncyFalloutPlane)) {
+        pref::get(pref::BoolPref::BouncyFalloutPlane) && mkb::stagedef != nullptr) {
         float ball_y = mkb::balls[mkb::curr_player_idx].pos.y;
         float fallout_plane = mkb::stagedef->fallout->y;
         if (ball_y < fallout_plane) {
-            if (mkb::balls[mkb::curr_player_idx].vel.y < 0)
-                mkb::balls[mkb::curr_player_idx].vel.y *= -1;
             mkb::balls[mkb::curr_player_idx].vel.y =
-                ABS(mkb::balls[mkb::curr_player_idx].vel.y) +
-                ABS(mkb::balls[mkb::curr_player_idx].vel.y * 0.075);
+                ABS(mkb::balls[mkb::curr_player_idx].vel.y) * 1.05;
         }
     }
 }
