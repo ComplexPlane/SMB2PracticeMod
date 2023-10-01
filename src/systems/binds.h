d7c031a453f0c42990b21dfd3539ed7b38f014ee
rehtrop <kingretrop@gmail.com>
2023-10-01 02:13:27 -0500

    removed unused functions from header file

--- src/systems/binds.h
+++ src/systems/binds.h
@@ -13,8 +13,6 @@ enum class EncodingType {
 bool bind_pressed(u8 bind_id, bool priority = false);
 EncodingType get_encoding_type();
 u8 get_current_encoding();
-u8 get_input1(u8 bind_id);
-u8 get_input2(u8 bind_id);
 char* get_bind_str(u8 bind_id);
 
 void init();
