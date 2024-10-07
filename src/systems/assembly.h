78092b85ac2607c8d4e950e9c8ba42a01e9b0783
ComplexPlane <46565903+ComplexPlane@users.noreply.github.com>
2024-10-07 19:40:51 -0400

    Remove main_loop_assembly asm func

--- src/systems/assembly.h
+++ src/systems/assembly.h
@@ -10,13 +10,6 @@ extern "C" {
 void custom_titlescreen_text_color();
 void full_debug_text_color();
 
-// main.cpp
-void start_main_loop_assembly();
-
-// Functions accessed by assembly overwrites
-// main.cpp
-void tick();
-
 // Full color to draw debug text in, if not zero
 // Normally, the debug text drawing function uses a single byte for the color in the form RRGGBBAA
 extern mkb::GXColor debug_text_color;
