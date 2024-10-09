fe68f111746bb58f713c117dae660f9e5859f2a7
ComplexPlane <46565903+ComplexPlane@users.noreply.github.com>
2024-10-09 11:24:04 -0400

    Avoid Rust formatting functions for code size

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,6 @@ mod relutil;
 use core::{
     alloc::{GlobalAlloc, Layout},
     ffi::c_char,
-    fmt::Write,
 };
 
 use panic_halt as _;
@@ -30,18 +29,19 @@ unsafe fn init() {
     heap::HEAP.init();
 
     log!("Hello World from Rust!");
-    log!("Available space: {}", heap::HEAP.get_free_space());
+    log!("Available space: %d", heap::HEAP.get_free_space());
     let alloc1 = heap::HEAP.alloc(Layout::new::<[u8; 1024]>());
-    log!("Available space 1: {}", heap::HEAP.get_free_space());
+    log!("Available space 1: %d", heap::HEAP.get_free_space());
     let alloc2 = heap::HEAP.alloc(Layout::new::<[u8; 327]>());
-    log!("Available space 2: {}", heap::HEAP.get_free_space());
+    log!("Available space 2: %d", heap::HEAP.get_free_space());
     let alloc3 = heap::HEAP.alloc(Layout::new::<[u8; 512]>());
-    log!("Available space 3: {}", heap::HEAP.get_free_space());
+    log!("Available space 3: %d", heap::HEAP.get_free_space());
 
     heap::HEAP.dealloc(alloc1, Layout::new::<[u8; 1024]>());
-    log!("Available space 4: {}", heap::HEAP.get_free_space());
+    log!("Available space 4: %d", heap::HEAP.get_free_space());
     heap::HEAP.dealloc(alloc3, Layout::new::<[u8; 512]>());
-    log!("Available space 5: {}", heap::HEAP.get_free_space());
+    log!("Available space 5: %d", heap::HEAP.get_free_space());
     heap::HEAP.dealloc(alloc2, Layout::new::<[u8; 327]>());
-    log!("Available space 6: {}", heap::HEAP.get_free_space());
+    log!("Available space 6: %d", heap::HEAP.get_free_space());
+    log!("A random float: %f %f", 3.2, -3.2);
 }
--- src/log.rs
+++ src/log.rs
@@ -2,17 +2,23 @@ use core::ffi::c_char;
 
 // TODO get this from bindgen
 extern "C" {
-    pub fn g_very_similar_to_puts(s: *const c_char);
+    pub fn OSReport(format: *const c_char, ...) -> i32;
 }
 
+// Wrap OSReport() because Rust's formatting functions have a large code size footprint (especially
+// for floats). It would be cool if there was some safe macro wrapper around printf-style
+// formatting.
 #[macro_export]
 macro_rules! log {
-    ($($arg:tt)*) => {{
-        let mut str = ArrayString::<512>::from("[pracmod] ").unwrap();
-        write!(&mut str, $($arg)*).unwrap();
-        str.push('\0');
+    ($fmt:expr $(, $($arg:tt)*)?) => {{
+        let mut c_fmt = ArrayString::<512>::from("[pracmod] ").unwrap();
+        c_fmt.push_str($fmt);
+        c_fmt.push_str("\n\0");
+
+        // I'm not sure it's possible to expand arg in a safe context
+        #[allow(clippy::macro_metavars_in_unsafe)]
         unsafe {
-            log::g_very_similar_to_puts(str.as_ptr() as *const c_char);
+            log::OSReport(c_fmt.as_ptr() as *const c_char $(, $($arg)*)?);
         }
     }};
 }
