785e85b9a7a179f77fabd43837bc054e936d88f4
ComplexPlane <46565903+ComplexPlane@users.noreply.github.com>
2024-10-12 19:41:12 -0400

    Get cool tramp! macro working!

--- src/app.rs
+++ src/app.rs
@@ -6,15 +6,9 @@ use critical_section::Mutex;
 use crate::hook;
 
 pub static APP_CONTEXT: Mutex<RefCell<AppContext>> = Mutex::new(RefCell::new(AppContext {
-    process_inputs_tramp: hook::Tramp::new(),
-    padread_tramp: hook::PadReadTramp::new(),
-    oslink_tramp: hook::OSLinkTramp::new(),
-    game_ready_init_tramp: hook::GameReadyInitTramp::new(),
+    padread_tramp: hook::PADReadTramp::new(),
 }));
 
 pub struct AppContext {
-    pub process_inputs_tramp: hook::Tramp,
-    pub padread_tramp: hook::PadReadTramp,
-    pub oslink_tramp: hook::OSLinkTramp,
-    pub game_ready_init_tramp: hook::GameReadyInitTramp,
+    pub padread_tramp: hook::PADReadTramp,
 }
--- src/hook.rs
+++ src/hook.rs
@@ -1,190 +1,59 @@
-use core::borrow::BorrowMut;
-use core::ffi::c_void;
 use core::mem::transmute;
 use core::ptr::null;
+use paste::paste;
 
 use crate::app::{self, AppContext};
+use crate::mkb;
 use crate::patch;
-use crate::{mkb, relutil};
 
-pub struct Tramp {
-    instrs: [u32; 2],
-    dest: Option<extern "C" fn()>,
-}
+macro_rules! tramp {
+    ($name:ident $(, $argname:ident: $arg:ty)* => $ret:ty, $their_func:expr, $our_func:expr) => {
+        pub struct $name {
+            instrs: [u32; 2],
+            chained_func: Option<extern "C" fn($($arg,)*) -> $ret>,
+            their_func: unsafe extern "C" fn($($arg,)*) -> $ret,
+            our_func_c: extern "C" fn($($arg,)*) -> $ret,
+        }
 
-impl Tramp {
-    pub const fn new() -> Self {
-        Self {
-            instrs: [0, 0],
-            dest: None,
+        paste! {
+            impl $name {
+                pub const fn new() -> Self {
+                    Self {
+                        instrs: [0, 0],
+                        chained_func: None,
+                        their_func: $their_func,
+                        our_func_c: Self::[<$name:snake _c_hook>],
+                    }
+                }
+
+                pub fn hook(&mut self) {
+                    unsafe {
+                        let mut chained_func_addr = null();
+                        patch::hook_function(
+                            self.their_func as *mut u32,
+                            self.our_func_c as *mut u32,
+                            &mut self.instrs,
+                            &mut chained_func_addr,
+                        );
+                        self.chained_func = Some(transmute(chained_func_addr));
+                    }
+                }
+
+                pub fn call(&self $(, $argname: $arg)*) -> $ret {
+                    (self.chained_func.unwrap())($($argname,)*)
+                }
+
+                extern "C" fn [<$name:snake _c_hook>]($($argname: $arg, )*) -> $ret {
+                    critical_section::with(|cs| {
+                        let mut cx = app::APP_CONTEXT.borrow(cs).borrow_mut();
+                        $our_func($($argname, )* &mut cx)
+                    })
+                }
+            }
         }
     }
-
-    pub unsafe fn hook(&mut self, func: unsafe extern "C" fn(), target: extern "C" fn()) {
-        let mut dest = null();
-        patch::hook_function(
-            func as *mut u32,
-            target as *mut u32,
-            &mut self.instrs,
-            &mut dest,
-        );
-        self.dest = Some(transmute(dest));
-    }
-
-    pub fn call(&self) {
-        (self.dest.unwrap())();
-    }
 }
 
-pub struct PadReadTramp {
-    instrs: [u32; 2],
-    dest: Option<extern "C" fn(*mut mkb::PADStatus)>,
-}
-
-impl PadReadTramp {
-    pub const fn new() -> Self {
-        Self {
-            instrs: [0, 0],
-            dest: None,
-        }
-    }
-
-    pub unsafe fn hook(
-        &mut self,
-        func: unsafe extern "C" fn(statuses: *mut mkb::PADStatus),
-        target: extern "C" fn(statuses: *mut mkb::PADStatus),
-    ) {
-        let mut dest = null();
-        patch::hook_function(
-            func as *mut u32,
-            target as *mut u32,
-            &mut self.instrs,
-            &mut dest,
-        );
-        self.dest = Some(transmute(dest));
-    }
-
-    pub fn call(&self, statuses: *mut mkb::PADStatus) {
-        (self.dest.unwrap())(statuses);
-    }
-}
-
-extern "C" fn padread_c_hook(statuses: *mut mkb::PADStatus) {
-    critical_section::with(|cs| {
-        let mut cx = app::APP_CONTEXT.borrow(cs).borrow_mut();
-        padread_hook(&mut cx, statuses);
-    });
-}
-
-fn padread_hook(cx: &mut AppContext, statuses: *mut mkb::PADStatus) {
-    cx.padread_tramp.call(statuses);
-}
-
-pub struct OSLinkTramp {
-    instrs: [u32; 2],
-    dest: Option<
-        extern "C" fn(rel_buffer: *mut mkb::OSModuleHeader, bss_buffer: *mut c_void) -> bool,
-    >,
-}
-
-impl OSLinkTramp {
-    pub const fn new() -> Self {
-        Self {
-            instrs: [0, 0],
-            dest: None,
-        }
-    }
-
-    pub unsafe fn hook(
-        &mut self,
-        func: unsafe extern "C" fn(
-            rel_buffer: *mut mkb::OSModuleHeader,
-            bss_buffer: *mut c_void,
-        ) -> bool,
-        target: extern "C" fn(
-            rel_buffer: *mut mkb::OSModuleHeader,
-            bss_buffer: *mut c_void,
-        ) -> bool,
-    ) {
-        let mut dest = null();
-        patch::hook_function(
-            func as *mut u32,
-            target as *mut u32,
-            &mut self.instrs,
-            &mut dest,
-        );
-        self.dest = Some(transmute(dest));
-    }
-
-    pub fn call(&self, rel_buffer: *mut mkb::OSModuleHeader, bss_buffer: *mut c_void) -> bool {
-        (self.dest.unwrap())(rel_buffer, bss_buffer)
-    }
-}
-
-extern "C" fn oslink_c_hook(rel_buffer: *mut mkb::OSModuleHeader, bss_buffer: *mut c_void) -> bool {
-    critical_section::with(|cs| {
-        let mut cx = app::APP_CONTEXT.borrow(cs).borrow_mut();
-        oslink_hook(rel_buffer, bss_buffer, &mut cx)
-    })
-}
-
-fn oslink_hook(
-    rel_buffer: *mut mkb::OSModuleHeader,
-    bss_buffer: *mut c_void,
-    cx: &mut AppContext,
-) -> bool {
-    let ret = cx.oslink_tramp.call(rel_buffer, bss_buffer);
-
-    let module_id = unsafe { (*rel_buffer).info.id };
-    let module_id = relutil::ModuleId::try_from(module_id).unwrap();
-
-    if module_id == relutil::ModuleId::MainGame {
-        unsafe {
-            cx.game_ready_init_tramp
-                .hook(mkb::smd_game_ready_init, game_ready_init_c_hook);
-        }
-    }
-
-    ret
-}
-
-pub struct GameReadyInitTramp {
-    instrs: [u32; 2],
-    dest: Option<extern "C" fn()>,
-}
-
-impl GameReadyInitTramp {
-    pub const fn new() -> Self {
-        Self {
-            instrs: [0, 0],
-            dest: None,
-        }
-    }
-
-    pub unsafe fn hook(&mut self, func: unsafe extern "C" fn(), target: extern "C" fn()) {
-        let mut dest = null();
-        patch::hook_function(
-            func as *mut u32,
-            target as *mut u32,
-            &mut self.instrs,
-            &mut dest,
-        );
-        self.dest = Some(transmute(dest));
-    }
-
-    pub fn call(&self) {
-        (self.dest.unwrap())()
-    }
-}
-
-extern "C" fn game_ready_init_c_hook() {
-    critical_section::with(|cs| {
-        let mut cx = app::APP_CONTEXT.borrow(cs).borrow_mut();
-        game_ready_init_hook(&mut cx)
-    })
-}
-
-fn game_ready_init_hook(cx: &mut AppContext) {
-    // TODO stuff here
-    cx.game_ready_init_tramp.call();
-}
+tramp!(PADReadTramp, status: *mut mkb::PADStatus => u32, mkb::PADRead, |statuses, cx: &mut AppContext| {
+    cx.padread_tramp.call(statuses)
+});
