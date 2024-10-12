90a428a9e674cb03ff6d3192e0a7ce0fb02f7261
ComplexPlane <46565903+ComplexPlane@users.noreply.github.com>
2024-10-11 21:10:47 -0400

    Write new Makefile

--- bindgen.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-IFS=$'\n\t'
-
-script_dir=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
-cd "$script_dir"
-
-bindgen cppsrc/mkb/mkb2_ghidra.h --use-core -o src/mkb.rs -- --target=powerpc-unknown-eabi
