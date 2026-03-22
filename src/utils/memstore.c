b24f3d69151bfb40555c7cbb124ddb003a5969b0
ComplexPlane <46565903+ComplexPlane@users.noreply.github.com>
2026-03-22 18:19:05 -0400

    Simpler memstore experimentation

---
+++ src/utils/memstore.c
@@ -0,0 +1,55 @@
+#include "mkb/mkb.h"
+#include "systems/heap.h"
+
+struct Store {
+    u32 size;
+    u32 pos;
+    void *buf;
+};
+
+void StoreComputeSize(Store *store, void *ptr, u32 size) {
+    store->size += size;
+}
+
+void StoreSave(Store *store, void *ptr, u32 size) {
+    mkb::memcpy((u8 *)store->buf + store->pos, ptr, size);
+    store->pos += size;
+}
+
+void StoreLoad(Store *store, void *ptr, u32 size) {
+    mkb::memcpy(ptr, (u8 *)store->buf + store->pos, size);
+    store->pos += size;
+}
+
+typedef void (*StoreFunc)(Store *store, void *ptr, u32 size);
+
+void PassOverRegions(StoreFunc f, Store *store) {
+    f(store, &mkb::balls[0], sizeof(mkb::balls[0]));
+    f(store, &mkb::sub_mode, sizeof(mkb::sub_mode));
+    f(store, &mkb::mode_info.stage_time_frames_remaining, 
+            sizeof(mkb::mode_info.stage_time_frames_remaining));
+    f(store, reinterpret_cast<void*>(0x8054E03C), 0xe0);  // Camera region
+    f(store, reinterpret_cast<void*>(0x805BD830), 0x1c);  // Some physics region
+    f(store, &mkb::mode_info.g_ball_mode, sizeof(mkb::mode_info.g_ball_mode));
+    f(store, mkb::g_camera_standstill_counters, sizeof(mkb::g_camera_standstill_counters));
+}
+
+struct GameState {
+    Store store;
+};
+
+void GameState_Save(GameState *state) {
+    Store store;
+    PassOverRegions(StoreComputeSize, &store);
+    store.buf = heap::alloc(state->store.size);
+    if (store.buf == nullptr) {
+        return;
+    }
+
+    PassOverRegions(StoreSave, &store);
+    store.pos = 0; // Ready to load
+    if (state->store.buf != nullptr) {
+        heap::free(state-.store.buf);
+    }
+    state->store = store;
+}
