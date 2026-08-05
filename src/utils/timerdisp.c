48b444af69e37c5a8e6c295b23dc0fc2b1d44356
ComplexPlane <46565903+ComplexPlane@users.noreply.github.com>
2026-08-05 18:23:48 -0400

    Port timerdisp

---
+++ src/utils/timerdisp.c
@@ -0,0 +1,140 @@
+#include "timerdisp.h"
+
+#include "utils/draw.h"
+
+static constexpr u32 SECOND_FRAMES = 60;
+static constexpr u32 MINUTE_FRAMES = SECOND_FRAMES * 60;
+static constexpr u32 HOUR_FRAMES = MINUTE_FRAMES * 60;
+
+static constexpr s32 X = 378;
+static constexpr s32 Y = 24;
+
+void timerdisp_draw_timer(u32 pos_x,
+                          u32 pos_y,
+                          u32 text_offset,
+                          const char *prefix,
+                          s32 frames_1,
+                          s32 frames_2,
+                          bool show_second_argument,
+                          bool show_seconds_only,
+                          mkb_GXColor color) {
+    bool positive = frames_1 >= 0;
+    if (!positive) frames_1 = -frames_1;
+    const char *sign = positive ? "" : "-";
+
+    u32 hours_1 = frames_1 / HOUR_FRAMES;
+    u32 minutes_1 = frames_1 % HOUR_FRAMES / MINUTE_FRAMES;
+    u32 seconds_1 = frames_1 % MINUTE_FRAMES / SECOND_FRAMES;
+    u32 centiseconds_1 = (frames_1 % SECOND_FRAMES) * 100 / 60;
+
+    u32 hours_2 = frames_2 / HOUR_FRAMES;
+    u32 minutes_2 = frames_2 % HOUR_FRAMES / MINUTE_FRAMES;
+    u32 seconds_2 = frames_2 % MINUTE_FRAMES / SECOND_FRAMES;
+    u32 centiseconds_2 = (frames_2 % SECOND_FRAMES) * 100 / 60;
+
+    s32 A = pos_x;
+    s32 a = A + text_offset;
+    s32 b = Y + (pos_y) * 16;
+
+    if (show_second_argument == true) {
+        if (hours_1 > 0) {
+            if (hours_2 > 0) {
+                draw_debug_text(A, b, color, prefix);
+                draw_debug_text(a, b, color, "%s%d:%02d:%02d.%02d (%s%d:%02d:%02d.%02d)", sign,
+                                hours_1, minutes_1, seconds_1, centiseconds_1, sign, hours_2,
+                                minutes_2, seconds_2, centiseconds_2);
+            } else if (minutes_2 > 0) {
+                draw_debug_text(A, b, color, prefix);
+                draw_debug_text(a, b, color, "%s%d:%02d:%02d.%02d (%s%02d:%02d.%02d)", sign,
+                                hours_1, minutes_1, seconds_1, centiseconds_1, sign, minutes_2,
+                                seconds_2, centiseconds_2);
+            } else {
+                draw_debug_text(A, b, color, prefix);
+                draw_debug_text(a, b, color, "%s%d:%02d:%02d.%02d (%s%02d.%02d)", sign, hours_1,
+                                minutes_1, seconds_1, centiseconds_1, sign, seconds_2,
+                                centiseconds_2);
+            }
+        } else if (minutes_1 > 0) {
+            if (hours_2 > 0) {
+                draw_debug_text(A, b, color, prefix);
+                draw_debug_text(a, b, color, "%s%02d:%02d.%02d (%s%d:%02d:%02d.%02d)", sign,
+                                minutes_1, seconds_1, centiseconds_1, sign, hours_2, minutes_2,
+                                seconds_2, centiseconds_2);
+            } else if (minutes_2 > 0) {
+                draw_debug_text(A, b, color, prefix);
+                draw_debug_text(a, b, color, "%s%02d:%02d.%02d (%s%02d:%02d.%02d)", sign, minutes_1,
+                                seconds_1, centiseconds_1, sign, minutes_2, seconds_2,
+                                centiseconds_2);
+            } else {
+                draw_debug_text(A, b, color, prefix);
+                draw_debug_text(a, b, color, "%s%02d:%02d.%02d (%s%02d.%02d)", sign, minutes_1,
+                                seconds_1, centiseconds_1, sign, seconds_2, centiseconds_2);
+            }
+
+        } else {
+            if (hours_2 > 0) {
+                draw_debug_text(A, b, color, prefix);
+                draw_debug_text(a, b, color, "%s%d.%02d (%s%d:%02d:%02d.%02d)", sign, seconds_1,
+                                centiseconds_1, sign, hours_2, minutes_2, seconds_2,
+                                centiseconds_2);
+            } else if (minutes_2 > 0) {
+                draw_debug_text(A, b, color, prefix);
+                draw_debug_text(a, b, color, "%s%d.%02d (%s%d:%02d.%02d)", sign, seconds_1,
+                                centiseconds_1, sign, minutes_2, seconds_2, centiseconds_2);
+            } else {
+                draw_debug_text(A, b, color, prefix);
+                draw_debug_text(a, b, color, "%s%d.%02d (%s%d.%02d)", sign, seconds_1,
+                                centiseconds_1, sign, seconds_2, centiseconds_2);
+            }
+        }
+    } else {
+        if (hours_1 > 0 && !show_seconds_only) {
+            draw_debug_text(A, b, color, prefix);
+            draw_debug_text(a, b, color, "%s%d:%02d:%02d.%02d", sign, hours_1, minutes_1, seconds_1,
+                            centiseconds_1);
+        } else if (minutes_1 > 0 && !show_seconds_only) {
+            draw_debug_text(A, b, color, prefix);
+            draw_debug_text(a, b, color, "%s%02d:%02d.%02d", sign, minutes_1, seconds_1,
+                            centiseconds_1);
+        } else {
+            u32 total_seconds_1 =
+                seconds_1 + (minutes_1 * MINUTE_FRAMES + hours_1 * HOUR_FRAMES) / SECOND_FRAMES;
+            draw_debug_text(A, b, color, prefix);
+            draw_debug_text(a, b, color, "%s%02d.%02d", sign, total_seconds_1, centiseconds_1);
+        }
+    }
+}
+
+void timerdisp_draw_subtick_timer(s32 frames,
+                                  const char *prefix,
+                                  u32 row,
+                                  mkb_GXColor color,
+                                  bool show_minutes,
+                                  u32 framesave,
+                                  bool extra_precision) {
+    bool positive = frames >= 0;
+    if (!positive) frames = -frames;
+    const char *sign = positive ? "" : "-";
+
+    u32 hours = frames / HOUR_FRAMES;
+    u32 minutes = frames % HOUR_FRAMES / MINUTE_FRAMES;
+    u32 seconds = frames % MINUTE_FRAMES / SECOND_FRAMES;
+    u32 milliseconds = ((frames % SECOND_FRAMES) * 100 + framesave) / 6;  // 3 digit
+    u32 extra = (((frames % SECOND_FRAMES) * 100 + framesave) * 10) / 6;  // 4 digit
+
+    s32 y = Y + row * 16;
+
+    u32 total_seconds = seconds + (minutes * MINUTE_FRAMES + hours * HOUR_FRAMES) / SECOND_FRAMES;
+    draw_debug_text(X, y, color, prefix);
+    if (extra_precision) {
+        draw_debug_text(X + 48, y, color, "%s%02d.%04d", sign, total_seconds, extra);
+    } else {
+        draw_debug_text(X + 48, y, color, "%s%02d.%03d", sign, total_seconds, milliseconds);
+    }
+}
+
+void timerdisp_draw_percentage(s32 fsave, const char *prefix, u32 row, mkb_GXColor color) {
+    s32 y = Y + row * 16;
+    draw_debug_text(X, y, color, prefix);
+    draw_debug_text(X + 48, y, color, "%2d%", fsave);
+}
--- src/utils/timerdisp.h
+++ src/utils/timerdisp.h
@@ -2,11 +2,20 @@
 
 #include "utils/base.h"
 
-namespace timerdisp {
-
-void draw_timer(u32 pos_x, u32 pos_y, u32 text_offset, const char* prefix, s32 frames_1, s32 frames_2, bool show_second_argument, bool show_seconds_only, mkb::GXColor color);
-void draw_subtick_timer(s32 frames, const char* prefix, u32 row, mkb::GXColor color,
-                        bool show_minutes, u32 framesave, bool extra_precision);
-void draw_percentage(s32 fsave, const char* prefix, u32 row, mkb::GXColor color);
-
-}  // namespace timerdisp
+void timerdisp_draw_timer(u32 pos_x,
+                          u32 pos_y,
+                          u32 text_offset,
+                          const char *prefix,
+                          s32 frames_1,
+                          s32 frames_2,
+                          bool show_second_argument,
+                          bool show_seconds_only,
+                          GXColor color);
+void timerdisp_draw_subtick_timer(s32 frames,
+                                  const char *prefix,
+                                  u32 row,
+                                  GXColor color,
+                                  bool show_minutes,
+                                  u32 framesave,
+                                  bool extra_precision);
+void timerdisp_draw_percentage(s32 fsave, const char *prefix, u32 row, GXColor color);
