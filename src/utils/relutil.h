6ed3d0092ba37d45c843093978b1c52de51e2ebc
ComplexPlane <46565903+ComplexPlane@users.noreply.github.com>
2026-08-05 17:35:57 -0400

    Port relutil

--- src/utils/relutil.h
+++ src/utils/relutil.h
@@ -1,20 +1,27 @@
 #pragma once
 
-namespace relutil {
+#include "base.h"
 
-enum class ModuleId {
-    Dol = 0,
-    MainLoop = 1,
-    MainGame = 2,
-    SelNgc = 3,
-    WorkshopMod = 100,
-    PracticeMod = 101,
+typedef enum RelId RelId;
+enum RelId {
+    RelId_Dol = 0,
+    RelId_MainLoop = 1,
+    RelId_MainGame = 2,
+    RelId_SelNgc = 3,
+    RelId_WorkshopMod = 100,
+    RelId_PracticeMod = 101,
 };
 
-/*
+/**
  * Returns one past the last address of relocation data in mainloop.rel which we are free to
  * overwrite.
  */
-void* compute_mainloop_reldata_boundary();
+void* rel_compute_mainloop_reldata_boundary(void* start);
 
-}  // namespace relutil
+/**
+ * Adjusts a pointer to account for differences in REL load locations compared to vanilla.
+ *
+ * RELs will be loaded at different addresses when using the merge-heaps patch for example.
+ * It is not necessary to "relocate" symbols which appear in mkb2.us.lst .
+ */
+void* rel_relocate_addr(u32 vanilla_addr);
