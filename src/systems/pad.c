91012f4cb0dcde4418cb531e613275d1522ef3fc
ComplexPlane <46565903+ComplexPlane@users.noreply.github.com>
2026-08-05 22:26:56 -0400

    Port pad

---
+++ src/systems/pad.c
@@ -0,0 +1,185 @@
+#include "systems/pad.h"
+
+#include "utils/base.h"
+
+static constexpr u32 DIR_REPEAT_PERIOD = 3;
+static constexpr u32 DIR_REPEAT_WAIT = 14;
+
+static bool s_exclusive_mode;
+static bool s_exclusive_mode_request;
+
+static mkb_AnalogInputGroup s_merged_analog_inputs;
+static mkb_DigitalInputGroup s_merged_digital_inputs;
+static mkb_AnalogInputGroup s_analog_inputs[4];
+static mkb_PadStatusGroup s_pad_status_groups[4];
+
+static u8 s_dir_down_time[8];
+
+bool pad_button_down(mkb_PadDigitalInput digital_input, bool priority) {
+    return (!s_exclusive_mode || priority) && (s_merged_digital_inputs.raw & digital_input);
+}
+
+bool pad_button_pressed(mkb_PadDigitalInput digital_input, bool priority) {
+    return (!s_exclusive_mode || priority) && s_merged_digital_inputs.pressed & digital_input;
+}
+
+bool pad_button_released(mkb_PadDigitalInput digital_input, bool priority) {
+    return (!s_exclusive_mode || priority) && s_merged_digital_inputs.released & digital_input;
+}
+
+bool pad_button_repeat(mkb_PadDigitalInput digital_input, bool priority) {
+    return (!s_exclusive_mode || priority) && s_merged_digital_inputs.repeated & digital_input;
+}
+
+bool pad_analog_down(mkb_PadAnalogInput analog_input, bool priority) {
+    return (!s_exclusive_mode || priority) && s_merged_analog_inputs.raw & analog_input;
+}
+
+bool pad_analog_pressed(mkb_PadAnalogInput analog_input, bool priority) {
+    return (!s_exclusive_mode || priority) && s_merged_analog_inputs.pressed & analog_input;
+}
+
+bool pad_analog_released(mkb_PadAnalogInput analog_input, bool priority) {
+    return (!s_exclusive_mode || priority) && s_merged_analog_inputs.released & analog_input;
+}
+
+bool pad_button_chord_pressed(mkb_PadDigitalInput btn1, mkb_PadDigitalInput btn2, bool priority) {
+    return (pad_button_down(btn1, priority) && pad_button_pressed(btn2, priority)) ||
+           (pad_button_pressed(btn1, priority) && pad_button_down(btn2, priority));
+}
+
+bool pad_analog_chord_pressed(mkb_PadDigitalInput analog1,
+                              mkb_PadDigitalInput analog2,
+                              bool priority) {
+    return (pad_analog_down(analog1, priority) && pad_analog_pressed(analog2, priority)) ||
+           (pad_analog_pressed(analog1, priority) && pad_analog_down(analog2, priority));
+}
+
+s32 pad_get_cstick_dir(bool priority) {
+    bool left = pad_analog_down(mkb_PAI_CSTICK_LEFT, priority);
+    bool right = pad_analog_down(mkb_PAI_CSTICK_RIGHT, priority);
+    bool up = pad_analog_down(mkb_PAI_CSTICK_UP, priority);
+    bool down = pad_analog_down(mkb_PAI_CSTICK_DOWN, priority);
+
+    if (up && left) return DIR_UPLEFT;
+    if (up && right) return DIR_UPRIGHT;
+    if (down && left) return DIR_DOWNLEFT;
+    if (down && right) return DIR_DOWNRIGHT;
+    if (up) return DIR_UP;
+    if (down) return DIR_DOWN;
+    if (left) return DIR_LEFT;
+    if (right) return DIR_RIGHT;
+    return DIR_NONE;
+}
+
+bool pad_dir_down(Dir dir, bool priority) {
+    switch (dir) {
+    case DIR_UP: {
+        return pad_button_down(mkb_PAD_BUTTON_UP, priority) ||
+               pad_analog_down(mkb_PAI_LSTICK_UP, priority);
+    }
+    case DIR_LEFT: {
+        return pad_button_down(mkb_PAD_BUTTON_LEFT, priority) ||
+               pad_analog_down(mkb_PAI_LSTICK_LEFT, priority);
+    }
+    case DIR_RIGHT: {
+        return pad_button_down(mkb_PAD_BUTTON_RIGHT, priority) ||
+               pad_analog_down(mkb_PAI_LSTICK_RIGHT, priority);
+    }
+    case DIR_DOWN: {
+        return pad_button_down(mkb_PAD_BUTTON_DOWN, priority) ||
+               pad_analog_down(mkb_PAI_LSTICK_DOWN, priority);
+    }
+    default: {
+        return false;
+    }
+    }
+}
+
+bool pad_dir_pressed(Dir dir, bool priority) {
+    switch (dir) {
+    case DIR_UP: {
+        return pad_button_pressed(mkb_PAD_BUTTON_UP, priority) ||
+               pad_analog_pressed(mkb_PAI_LSTICK_UP, priority);
+    }
+    case DIR_LEFT: {
+        return pad_button_pressed(mkb_PAD_BUTTON_LEFT, priority) ||
+               pad_analog_pressed(mkb_PAI_LSTICK_LEFT, priority);
+    }
+    case DIR_RIGHT: {
+        return pad_button_pressed(mkb_PAD_BUTTON_RIGHT, priority) ||
+               pad_analog_pressed(mkb_PAI_LSTICK_RIGHT, priority);
+    }
+    case DIR_DOWN: {
+        return pad_button_pressed(mkb_PAD_BUTTON_DOWN, priority) ||
+               pad_analog_pressed(mkb_PAI_LSTICK_DOWN, priority);
+    }
+    default: {
+        return false;
+    }
+    }
+}
+
+bool pad_dir_repeat(Dir dir, bool priority) {
+    if (s_exclusive_mode && !priority) return false;
+
+    u32 t = s_dir_down_time[dir];
+    return pad_dir_pressed(dir, priority) ||
+           (t >= DIR_REPEAT_WAIT && ((t - DIR_REPEAT_WAIT) % DIR_REPEAT_PERIOD) == 0);
+}
+
+void pad_reset_dir_repeat() {
+    mkb_memset(s_dir_down_time, 0, sizeof(s_dir_down_time));
+}
+
+void pad_set_exclusive_mode(bool enabled) {
+    s_exclusive_mode_request = enabled;
+}
+
+bool pad_get_exclusive_mode() {
+    return s_exclusive_mode;
+}
+
+void pad_on_frame_start() {
+    if (s_exclusive_mode) {
+        // Restore previous controller inputs so new inputs can be computed correctly by the
+        // game
+        mkb_merged_analog_inputs = s_merged_analog_inputs;
+        mkb_merged_digital_inputs = s_merged_digital_inputs;
+        mkb_memcpy(mkb_pad_status_groups, s_pad_status_groups, sizeof(mkb_pad_status_groups));
+        mkb_memcpy(mkb_analog_inputs, s_analog_inputs, sizeof(mkb_analog_inputs));
+    }
+
+    // Only now do we honor the request to change into/out of exclusive mode
+    s_exclusive_mode = s_exclusive_mode_request;
+}
+
+static void update_dir_times() {
+    for (u32 dir = 0; dir < 8; dir++) {
+        if (pad_dir_down(dir, true)) {
+            s_dir_down_time[dir]++;
+            if (s_dir_down_time[dir] == 120) {
+                s_dir_down_time[dir] = 120 - DIR_REPEAT_PERIOD;
+            }
+        } else {
+            s_dir_down_time[dir] = 0;
+        }
+    }
+}
+
+void pad_tick() {
+    s_merged_analog_inputs = mkb_merged_analog_inputs;
+    s_merged_digital_inputs = mkb_merged_digital_inputs;
+    mkb_memcpy(s_pad_status_groups, mkb_pad_status_groups, sizeof(mkb_pad_status_groups));
+    mkb_memcpy(s_analog_inputs, mkb_analog_inputs, sizeof(mkb_analog_inputs));
+
+    if (s_exclusive_mode) {
+        // Zero controller inputs in the game
+        mkb_merged_analog_inputs = (mkb_AnalogInputGroup){};
+        mkb_merged_digital_inputs = (mkb_DigitalInputGroup){};
+        mkb_memset(mkb_pad_status_groups, 0, sizeof(mkb_pad_status_groups));
+        mkb_memset(mkb_analog_inputs, 0, sizeof(mkb_analog_inputs));
+    }
+
+    update_dir_times();
+}
--- src/systems/pad.h
+++ src/systems/pad.h
@@ -2,8 +2,6 @@
 
 #include "utils/base.h"
 
-namespace pad {
-
 typedef enum Dir Dir;
 enum Dir {
     DIR_UP,
@@ -17,37 +15,35 @@ enum Dir {
     DIR_NONE = -1,
 };
 
-void init();
+void pad_init();
 // Tick functions to be run at different points in the game loop
-void on_frame_start();
-void tick();  // Run this after controller inputs are read and processed by the game
+void pad_on_frame_start();
+void pad_tick();  // Run this after controller inputs are read and processed by the game
 
 // In exclusive mode, inputs only register
 // when passing `true` to the optional second argument of the input checking functions,
 // meanwhile the game sees zero inputs.
-void set_exclusive_mode(bool enabled);
-bool get_exclusive_mode();
+void pad_set_exclusive_mode(bool enabled);
+bool pad_get_exclusive_mode();
 
 // Simple wrappers about internal MKB2 bitfields. Represents OR-ed inputs of all controllers.
 
-// Accept a mkb::PadDigitalInput
-bool button_down(u16 digital_input, bool priority = false);
-bool button_pressed(u16 digital_input, bool priority = false);
-bool button_released(u16 digital_input, bool priority = false);
-bool button_repeat(mkb::PadDigitalInput digital_input, bool priority = false);
-bool button_chord_pressed(u16 btn1, u16 btn2, bool priority = false);
+// Accept a mkb_PadDigitalInput
+bool pad_button_down(u16 digital_input, bool priority);
+bool pad_button_pressed(u16 digital_input, bool priority);
+bool pad_button_released(u16 digital_input, bool priority);
+bool pad_button_repeat(mkb_PadDigitalInput digital_input, bool priority);
+bool pad_button_chord_pressed(u16 btn1, u16 btn2, bool priority);
 
-// Accept a mkb::PadAnalogInput
-bool analog_down(u16 analog_input, bool priority = false);
-bool analog_pressed(u16 analog_input, bool priority = false);
-bool analog_released(u16 analog_input, bool priority = false);
-bool analog_chord_pressed(u16 analog1, u16 analog2, bool priority = false);
+// Accept a mkb_PadAnalogInput
+bool pad_analog_down(u16 analog_input, bool priority);
+bool pad_analog_pressed(u16 analog_input, bool priority);
+bool pad_analog_released(u16 analog_input, bool priority);
+bool pad_analog_chord_pressed(u16 analog1, u16 analog2, bool priority);
 
-s32 get_cstick_dir(bool priority = false);
-bool dir_down(Dir dir, bool priority = false);     // Only works for cardinal directions
-bool dir_pressed(Dir dir, bool priority = false);  // Only works for cardinal directions
-bool dir_repeat(Dir dir, bool priority = false);   // Only works for cardinal directions
-void reset_dir_repeat();
-bool konami_pressed();
-
-}  // namespace pad
+s32 pad_get_cstick_dir(bool priority);
+bool pad_dir_down(Dir dir, bool priority);     // Only works for cardinal directions
+bool pad_dir_pressed(Dir dir, bool priority);  // Only works for cardinal directions
+bool pad_dir_repeat(Dir dir, bool priority);   // Only works for cardinal directions
+void pad_reset_dir_repeat();
+bool pad_konami_pressed();
